0e1d31b6580aa07bec3b255cde0f73b4375b491a
LeeTaeYun <lty0812@naver.com>
2023-12-30 17:20:19 +0900

    23/12/30 17:20 Update Linked Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{jsx,js,ts,tsx}"],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1160px',
+      'xl': '1280px',
+      '2xl': '1560px',
+    },
     extend: {
+      scrollbar:['#0085FF'],
       colors: {
         brand: "#F96162",
         search: "#2D5FFF",
+        Result:"#585858",
       },
       boxShadow: {
         up: "0 -4px 6px -1px rgba(0,0,0,0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
+        custom: "5px 5px 20px 0px rgba(0,0,0,0.75)",
+        detail:'0px 4px 4px 0px rgba(0,0,0,0.25)',
+        change:'4px 8px 5px 0px rgba(0,0,0,0.25)'
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
@@ -21,5 +33,14 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      // 스크롤바 variant 추가
+      scrollbar: ['rounded']
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar'),
+  ],
 };
